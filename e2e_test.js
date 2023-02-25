6e59a10d933d27f960314b0b4f0ec872d258f5a3
Stefano Ramponi <stefano.ramponi.lav@gmail.com>
2023-02-25 10:33:07 +0100

    fixed issue and moved to js instead of ts

--- e2e_test.js
+++ e2e_test.js
@@ -5,14 +5,15 @@ Before(({ I }) => {
     I.amOnPage('http://zero.webappsecurity.com/index.html')
 });
 
-Scenario('Login test',  ({ I }) => {
+Scenario('Login test',  ({ I , LoginPage }) => {
     I.click('#signin_button')
     I.seeElement('#login_form')
 
-    I.fillField('#user_login','invalid username')
-    I.fillField('#user_password','invalid password')
-    I.click('.btn-primary')
-
+    //I.fillField('#user_login','invalid username')
+    //I.fillField('#user_password','invalid password')
+    //I.click('.btn-primary')
+    LoginPage.submitLogin('invalid username', 'invalid password')
+    LoginPage.assertLoginform()
     I.seeElement('.alert-error')
 });
 
--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -3,20 +3,22 @@ const { I } = inject();
 module.exports = {
 
   // Set locators
+  fields: {
   loginform: "#login_form",
   username: "#user_login",
   password: "#user_password",
   submitButton: ".btn-primary",
+},
 
   // create methods
   submitLogin(username, password) {
-    I.fillField(this.username, username)
-    I.fillField(this.password, password)
+    I.fillField(this.fields.username, username)
+    I.fillField(this.fields.password, password)
 
-    I.click(this.submitButton)
-  }
+    I.click(this.fields.submitButton)
+  },
 
   assertLoginform() {
-    I.seeElement(this.loginform)
+    I.seeElement(this.fields.loginform)
   }
 }
